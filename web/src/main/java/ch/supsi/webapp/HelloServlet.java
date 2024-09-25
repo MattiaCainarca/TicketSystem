44f6777b53fcbdc2cc6f65007f859fa95be54fbf
MattiaCainarca <mattia.cainarca@gmail.com>
2024-09-25 15:39:56 +0200

    PP1 Servlet

---
+++ web/src/main/java/ch/supsi/webapp/HelloServlet.java
@@ -0,0 +1,41 @@
+package ch.supsi.webapp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(value = "/tickets")
+public class HelloServlet extends HttpServlet {
+
+    List<Ticket> tickets = new ArrayList<Ticket>();
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Ticket ticket = null;
+        if (req.getContentType().equals("application/json"))
+            ticket = mapper.readValue(req.getReader(), Ticket.class);
+        else if (req.getContentType().equals("application/x-www-form-urlencoded")) {
+            ticket = new Ticket(req.getParameter("title"),
+                    req.getParameter("description"),
+                    req.getParameter("author"));
+        } else
+            resp.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        tickets.add(ticket);
+        resp.setContentType(req.getContentType());
+        resp.getWriter().write(mapper.writeValueAsString(tickets));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().write(mapper.writeValueAsString(tickets));
+    }
+}

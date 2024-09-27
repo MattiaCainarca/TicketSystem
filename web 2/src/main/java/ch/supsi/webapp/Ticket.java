fc1d603b4e3c2ee99287eba47bd84edc000b804e
MattiaCainarca <mattia.cainarca@gmail.com>
2024-09-27 09:31:51 +0200

    PP1 - Servlet

---
+++ web 2/src/main/java/ch/supsi/webapp/Ticket.java
@@ -0,0 +1,40 @@
+package ch.supsi.webapp;
+
+public class Ticket {
+    private String title;
+    private String description;
+    private String author;
+
+    public Ticket() {
+    }
+
+    public Ticket(String title, String description, String author) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}

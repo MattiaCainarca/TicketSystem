f580fe8346fa9951444c723394ae76f11858facc
MattiaCainarca <mattia.cainarca@gmail.com>
2024-10-14 18:30:02 +0200

    PP3 - start spring

---
+++ src/main/java/ch/supsi/webapp/tickets/Ticket.java
@@ -0,0 +1,22 @@
+package ch.supsi.webapp.tickets;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class Ticket {
+    private String id;
+    private String title;
+    private String description;
+    private String author;
+
+    public Ticket(String title, String description, String author) {
+        this.id = UUID.randomUUID().toString();
+        this.title = title;
+        this.description = description;
+        this.author = author;
+    }
+}

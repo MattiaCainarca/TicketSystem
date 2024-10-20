1d9f84bdf65ff9bfb16115a6fe65158e4bf2dccf
MattiaCainarca <mattia.cainarca@gmail.com>
2024-10-20 12:08:11 +0200

    PP4 - Spring

--- src/main/java/ch/supsi/webapp/tickets/controller/TicketController.java
+++ src/main/java/ch/supsi/webapp/tickets/controller/TicketController.java
@@ -1,8 +1,8 @@
 package ch.supsi.webapp.tickets.controller;
 
-import ch.supsi.webapp.tickets.model.Success;
 import ch.supsi.webapp.tickets.model.Ticket;
 import ch.supsi.webapp.tickets.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +15,15 @@ import java.util.Optional;
 public class TicketController {
     private final TicketService ticketService;
 
+    @Autowired
     public TicketController(TicketService ticketService) {
         this.ticketService = ticketService;
     }
 
     @PostMapping("")
     public ResponseEntity<Ticket> postTicket(@RequestBody Ticket newTicket) {
-        return new ResponseEntity<>(ticketService.create(newTicket), HttpStatus.CREATED);
+        Ticket createdTicket = ticketService.create(newTicket);
+        return new ResponseEntity<>(createdTicket, HttpStatus.CREATED);
     }
 
     @GetMapping("")
@@ -30,20 +32,19 @@ public class TicketController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Ticket> getTicketById(@PathVariable String id) {
+    public ResponseEntity<Ticket> getTicketById(@PathVariable Long id) {
         Optional<Ticket> ticket = ticketService.getOne(id);
         return ticket.isPresent() ? new ResponseEntity<>(ticket.get(), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Ticket> updateTicket(@PathVariable String id, @RequestBody Ticket updatedTicket) {
+    public ResponseEntity<Ticket> updateTicket(@PathVariable Long id, @RequestBody Ticket updatedTicket) {
         return ticketService.update(id, updatedTicket) != null ? ResponseEntity.ok(updatedTicket) : ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Success> deleteTicket(@PathVariable String id) {
-        boolean removed = ticketService.delete(id);
-        return removed ? new ResponseEntity<>(Success.builder().success(true).build(), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Void> deleteTicket(@PathVariable Long id) {
+        return ticketService.delete(id) ? ResponseEntity.ok().build() : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
 
--- src/main/java/ch/supsi/webapp/tickets/service/TicketService.java
+++ src/main/java/ch/supsi/webapp/tickets/service/TicketService.java
@@ -1,7 +1,9 @@
 package ch.supsi.webapp.tickets.service;
 
 import ch.supsi.webapp.tickets.model.Ticket;
-import ch.supsi.webapp.tickets.repository.InMemoryRepository;
+import ch.supsi.webapp.tickets.model.User;
+import ch.supsi.webapp.tickets.repository.TicketRepository;
+import ch.supsi.webapp.tickets.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,29 +11,57 @@ import java.util.Optional;
 
 @Service
 public class TicketService {
-    private final InMemoryRepository repository;
+    private final TicketRepository ticketRepository;
+    private final UserRepository userRepository;
 
-    public TicketService(InMemoryRepository repository) {
-        this.repository = repository;
+    public TicketService(TicketRepository ticketRepository, UserRepository userRepository) {
+        this.ticketRepository = ticketRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Ticket> findAll() {
-        return repository.findAll();
+        return ticketRepository.findAll();
     }
 
-    public Optional<Ticket> getOne(String id) {
-        return repository.getOne(id);
+    public Optional<Ticket> getOne(Long id) {
+        return ticketRepository.findById(id);
     }
 
     public Ticket create(Ticket ticket) {
-        return repository.create(ticket);
+        Optional<User> existingUser = userRepository.findByUsername(ticket.getUser().getUsername());
+        if (existingUser.isPresent())
+            ticket.setUser(existingUser.get());
+        else
+            userRepository.save(ticket.getUser());
+        return ticketRepository.save(ticket);
     }
 
-    public Ticket update(String id, Ticket ticket) {
-        return repository.update(id, ticket);
+    public Ticket update(Long id, Ticket ticket) {
+        Optional<Ticket> updatedTicket = ticketRepository.findById(id);
+        if (updatedTicket.isPresent()) {
+            ticket.setId(id);
+            User user = checkUser(ticket.getUser());
+            ticket.setUser(user);
+            return ticketRepository.save(ticket);
+        } else
+            return null;
     }
 
-    public boolean delete(String id) {
-        return repository.delete(id);
+    public boolean delete(Long id) {
+        Optional<Ticket> ticket = ticketRepository.findById(id);
+        if (ticket.isPresent()) {
+            ticketRepository.delete(ticket.get());
+            return true;
+        }
+        return false;
+    }
+
+    private User checkUser(User user) {
+        Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
+        if (existingUser.isPresent()) {
+            user.setId(existingUser.get().getId());
+            return user;
+        } else
+            return userRepository.save(user);
     }
 }
